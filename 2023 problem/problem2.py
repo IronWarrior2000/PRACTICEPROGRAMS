a1b2f82b0ebfe3721f96248381713121ded1feb0
Micah Winters <45744442+Micahaha@users.noreply.github.com>
2024-09-23 06:58:59 -0400

    problem 2

---
+++ 2023 problem/problem2.py
@@ -0,0 +1,25 @@
+def victory_road():
+    num_steps = int(input('num steps: '))
+    danger_steps = int(input('dangerous steps count: '))
+    dangerous_steps_to_avoid = input('Type steps to avoid: ')
+
+    dangerous_steps = list(dangerous_steps.split())
+
+    count = 0
+
+    for i in range(0, num_steps):
+        step_path.append(i)
+
+    for i in range(0, num_steps):
+        if step_path[i] or step_path[i+1] == dangerous_steps:
+            count-=1
+        else:
+            count+=1
+    print(count)
+
+
+
+def main():
+    victory_road()
+
+main()
